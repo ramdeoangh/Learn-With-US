64bc6b16974b616e806f5ee491fae18495e7b204
ramdeo.angh <ramdeo.angh@gmail.com>
2020-12-23 22:42:36 +0530

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fe379af94657a0021e7d081",
+				"5fe379af94657a0021e7d083"
+			]
+		},
+		"5fe379af94657a0021e7d081": {
+			"id": "5fe379af94657a0021e7d081",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fe379af94657a0021e7d083": {
+			"id": "5fe379af94657a0021e7d083",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
